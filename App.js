d42258428d99198ae8a91730de4b24d9ee9ac71f
Hammad-23 <hsyed2633@gmail.com>
2020-10-29 10:04:41 -0700

    done

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View,ScrollView,TouchableOpacity,TextInput } from 'react-native';
 
 export default function App() {
+   
+  const [list,setList]=useState([])
+
+  const [fullName,setName]=useState('')
+
+  const  val=(enteredText)=>{
+    const name = (enteredText.nativeEvent.text)
+    setName(name)
+    console.log(fullName)
+   
+
+  }
+
+  const add=()=>{
+    const newList=[...list]
+    newList.push(fullName)
+    setList(newList)
+  }
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{marginTop:200}}>
+       <TextInput onChange={val} style={{borderWidth:1,maxWidth:200,minWidth:200,height:40,borderRadius:10}} placeholder=' Enter Name'/>
+       <TouchableOpacity onPress={add} style={{backgroundColor:'green',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}>
+         <Text style={{color:'white',marginTop:9,marginLeft:20}}>
+           ADD
+         </Text>
+       </TouchableOpacity>
+       <View style={{marginTop:30}}>
+         {list.map(function(item){
+           return(
+           <Text>{item}<TouchableOpacity style={{backgroundColor:'blue',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}><Text style={{color:'white',marginTop:9,marginLeft:20}}>Delete</Text></TouchableOpacity></Text>
+          
+           )
+         })}
+       </View>
+       </View>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 

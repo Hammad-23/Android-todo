aac7d058759358fd33497393963a6d6ae83b448c
Hammad-23 <hsyed2633@gmail.com>
2020-10-29 10:18:07 -0700

    todo add work done

--- App.js
+++ App.js
@@ -7,11 +7,13 @@ export default function App() {
   const [list,setList]=useState([])
 
   const [fullName,setName]=useState('')
+  
 
   const  val=(enteredText)=>{
     const name = (enteredText.nativeEvent.text)
     setName(name)
     console.log(fullName)
+    
    
 
   }
@@ -20,12 +22,13 @@ export default function App() {
     const newList=[...list]
     newList.push(fullName)
     setList(newList)
+    
   }
   return (
     <ScrollView>
     <View style={styles.container}>
       <View style={{marginTop:200}}>
-       <TextInput onChange={val} style={{borderWidth:1,maxWidth:200,minWidth:200,height:40,borderRadius:10}} placeholder=' Enter Name'/>
+       <TextInput  onChange={val} style={{borderWidth:1,maxWidth:200,minWidth:200,height:40,borderRadius:10}} placeholder=' Enter Name'/>
        <TouchableOpacity onPress={add} style={{backgroundColor:'green',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}>
          <Text style={{color:'white',marginTop:9,marginLeft:20}}>
            ADD
@@ -34,7 +37,9 @@ export default function App() {
        <View style={{marginTop:30}}>
          {list.map(function(item){
            return(
-           <Text>{item}<TouchableOpacity style={{backgroundColor:'blue',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}><Text style={{color:'white',marginTop:9,marginLeft:20}}>Delete</Text></TouchableOpacity></Text>
+           <Text style={{fontSize:20}}>{item}<TouchableOpacity style={{backgroundColor:'blue',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}><Text style={{color:'white',marginTop:9,marginLeft:20}}>Delete</Text></TouchableOpacity>
+           <TouchableOpacity style={{backgroundColor:'red',height:40,width:80,borderRadius:10,marginLeft:60,marginTop:10}}><Text style={{color:'white',marginTop:9,marginLeft:20}}>Edit</Text></TouchableOpacity>
+           </Text>
           
            )
          })}
